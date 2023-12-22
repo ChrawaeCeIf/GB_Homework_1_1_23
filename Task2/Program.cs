d0f514f9c64b096aad112ca36af067257de23e6d
Артём Глушков <artyom.glushkov.2000@bk.ru>
2023-12-22 19:26:28 +0300

    Complete a second task

---
+++ Task2/Program.cs
@@ -0,0 +1,30 @@
+﻿// Задача 2: Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причём X ≠ 0 и Y ≠ 0 и выдаёт номер координатной четверти плоскости, в которой находится эта точка.
+
+
+Console.Write("Enter the coordinate of point X: ");
+int X = int.Parse(Console.ReadLine());
+Console.Write("Enter the coordinate of point Y: ");
+int Y = int.Parse(Console.ReadLine());
+
+if (X == 0 || Y == 0)
+{
+    Console.WriteLine("Условие не выполняется");
+}
+else
+if (X > 0 && Y > 0)
+{
+    Console.WriteLine("The point is located in the first quadrant");
+}
+if (X < 0 && Y > 0)
+{
+    Console.WriteLine("The point is located in the second quadrant");
+}
+if (X < 0 && Y < 0)
+{
+    Console.WriteLine("The point is located in the third quadrant");
+}
+if (X > 0 && Y < 0)
+{
+    Console.WriteLine("The point is located in the fourth quadrant");
+}
